26f7d33d54c42c7ef6f22bb85948fb4dad0db7ca
LUIZ GUSTAVO PEREIRA <luiz.pereira4@alunos.sc.senac.br>
2026-03-18 15:42:53 -0300

    refactor: substituir buttons por IonButton com ícones

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,122 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
+  <IonPage>
+    <IonHeader>
+      <IonToolbar color="primary">
+        <IonTitle>Mini App de Tarefas</IonTitle>
+      </IonToolbar>
+    </IonHeader>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <IonContent class="ion-padding">
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
-  </ion-page>
+      <!-- CARD FORMULÁRIO -->
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>Nova Tarefa</IonCardTitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          <IonInput
+            label="Nome da tarefa"
+            label-placement="floating"
+            v-model="novaTarefa"
+            :clear-input="true"
+            placeholder="Ex: Estudar Vue.js"
+            :error-text="erroTarefa"
+            :class="{ 'ion-invalid ion-touched': erroTarefa }"
+          />
+
+          <IonButton
+            expand="block"
+            fill="solid"
+            color="primary"
+            @click="adicionar"
+          >
+            <IonIcon :icon="addOutline" slot="start" />
+            Adicionar
+          </IonButton>
+        </IonCardContent>
+      </IonCard>
+
+      <!-- CARD LISTA -->
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>
+            Minhas Tarefas ({{ tarefas.length }})
+          </IonCardTitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+
+          <IonList v-if="tarefas.length">
+            <IonItem v-for="(t, i) in tarefas" :key="i">
+              
+              <IonIcon
+                slot="start"
+                :icon="checkmarkCircleOutline"
+              />
+
+              <IonLabel>{{ t }}</IonLabel>
+
+              <IonButton
+                slot="end"
+                fill="clear"
+                color="danger"
+                @click="remover(i)"
+              >
+                <IonIcon :icon="trashOutline" />
+              </IonButton>
+
+            </IonItem>
+          </IonList>
+
+          <p
+            v-else
+            class="ion-text-center ion-padding"
+          >
+            Nenhuma tarefa cadastrada.
+          </p>
+
+        </IonCardContent>
+      </IonCard>
+
+    </IonContent>
+  </IonPage>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script setup>
+import { ref, computed } from 'vue'
+
+import {
+  IonPage, IonHeader, IonToolbar, IonTitle, IonContent,
+  IonList, IonItem, IonLabel, IonInput,
+  IonButton, IonIcon,
+  IonCard, IonCardHeader, IonCardTitle, IonCardContent
+} from '@ionic/vue'
+
+import {
+  addOutline,
+  trashOutline,
+  checkmarkCircleOutline
+} from 'ionicons/icons'
+
+// estados
+const novaTarefa = ref('')
+const tarefas = ref([])
+
+// validação
+const erroTarefa = computed(() =>
+  !novaTarefa.value.trim() ? 'Campo obrigatório' : ''
+)
+
+// funções
+function adicionar() {
+  if (erroTarefa.value) return
+
+  tarefas.value.push(novaTarefa.value)
+  novaTarefa.value = ''
+}
+
+function remover(index) {
+  tarefas.value.splice(index, 1)
+}
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
